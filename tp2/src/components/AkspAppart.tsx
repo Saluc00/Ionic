67dc01a0e3cba8662d1c51800b0197d9df0d9a97
Saluc00 <l.usereau@outlook.fr>
2020-10-17 17:51:11 +0200

    input modal

--- tp2/src/components/AkspAppart.tsx
+++ tp2/src/components/AkspAppart.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { IonModal, IonButton, IonContent } from '@ionic/react';
+import { IonModal, IonButton, IonContent, IonItem, IonLabel, IonInput, IonList } from '@ionic/react';
 
 const AskAppart: React.FC = () => {
   const [showModal, setShowModal] = useState(false);
@@ -8,7 +8,52 @@ const AskAppart: React.FC = () => {
     <IonContent>
       <IonModal isOpen={showModal} cssClass='my-custom-class'>
         <p>This is modal content</p>
+        <IonList>
+        <IonItem>
+          <IonLabel position="floating">
+            Adresse
+          </IonLabel>
+          <IonInput ></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="floating">
+            Prix
+          </IonLabel>
+          <IonInput ></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="floating">
+          Surface (m²)
+          </IonLabel>
+          <IonInput ></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="floating">
+          Prix de rénovation
+          </IonLabel>
+          <IonInput ></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="floating">
+          Loyer
+          </IonLabel>
+          <IonInput ></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="floating">
+          Temps de vacances
+          </IonLabel>
+          <IonInput ></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="floating">
+          Photos
+          </IonLabel>
+          <IonInput ></IonInput>
+        </IonItem>
+        <IonButton >Ajouter</IonButton>
         <IonButton onClick={() => setShowModal(false)}>Fermer</IonButton>
+        </IonList>
       </IonModal>
       <IonButton onClick={() => setShowModal(true)}>Ajouter Appartement</IonButton>
     </IonContent>
